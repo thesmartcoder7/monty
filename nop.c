e1b81acef7660ac0a07bf994bb81093ef03f972a
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-26 03:18:39 +0300

    chore: implement the nop opcode

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- does nothing
+  *@head: the head of the stack
+  *@counter: code line position
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
